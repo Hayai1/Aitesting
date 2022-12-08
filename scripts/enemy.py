42326babb8455f0076d60e45a6040c9cf17ed567
Kenshi <60287993+Hayai1@users.noreply.github.com>
2022-12-08 09:34:41 +0000

    ai seems to work now

--- scripts/enemy.py
+++ scripts/enemy.py
@@ -4,7 +4,6 @@ class Enemy(Character):
     def __init__(self,x, y, width, height,graph,imgPath,xVelocity=1):
         self.graph = graph
         self.ai = Ai(graph)
-        self.currentNode = graph.nodes[10]
         self.timeSinceLastSolve = 100
         self.nodePointer = 0
         self.frame = 0
@@ -13,26 +12,32 @@ class Enemy(Character):
         self.solve = False
         self.nextNode = None
         super().__init__(x, y, width, height,imgPath,xVelocity)
+        self.currentNode = self.graph.getNodeCloseTo(self)
+
     def draw(self, screen,scroll,tiles):
         #move based on the path
+        cancelCurrentPath = False
         self.movingLeft = False
         self.movingRight = False
         if self.path is not None:#if a path exists then move based on the path
             if self.currentNode is not None:
                 g = self.currentNode.getG(self.nextNode)
-                gx = g[0]
-                gy = g[1]
-                if gy is not 0:
-                    self.jump()
-                if self.nextNode.x > self.currentNode.x:
-                    self.movingLeft = False
-                    self.movingRight = True
-                elif self.nextNode.x < self.currentNode.x:
-                    self.movingRight = False
-                    self.movingLeft = True
-                self.frame += 1
-                self.frameToGetNextNode = 16*gx
-            if self.frame == self.frameToGetNextNode:#if frame is equal to the frame to get to the next node then:
+                if g is not None:     
+                    gx = g[0]
+                    gy = g[1]
+                    if gy is not 0 and self.currentNode.y > self.nextNode.y :
+                        self.jump()
+                    if self.nextNode.x > self.currentNode.x:
+                        self.movingLeft = False
+                        self.movingRight = True
+                    elif self.nextNode.x < self.currentNode.x:
+                        self.movingRight = False
+                        self.movingLeft = True
+                    self.frame += 1
+                    self.frameToGetNextNode = 16*abs(gx)
+                else:
+                    cancelCurrentPath = True
+            if self.frame >= self.frameToGetNextNode or cancelCurrentPath:#if frame is equal to the frame to get to the next node then:
                 #reset the frame and get the next node to visit
                 self.frame = 0
                 if self.nodePointer < len(self.path) - 1:#if the ai hasn't reached the last node in the path then get the next node to move too
@@ -60,9 +65,10 @@ class Enemy(Character):
         screen.blit(self.img, (self.rect.x - scroll[0],self.rect.y - scroll[1]))
         
     def update(self,player):
-        if self.solve and self.graph.getNodeCloseTo(self) is not self.graph.getNodeCloseTo(player):
+        if self.timeSinceLastSolve > 2 and self.graph.getNodeCloseTo(self) is not self.graph.getNodeCloseTo(player):
             self.solve = False
             self.path = self.ai.DrawPath(self.graph.getNodeCloseTo(self),player)
+            self.path.append(self.graph.getNodeCloseTo(player))
             self.timeSinceLastSolve = 0
             self.nodePointer = 1
             self.nextNode = self.path[self.nodePointer]
